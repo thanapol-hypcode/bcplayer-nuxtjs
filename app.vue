a1cb25f2f986d0fe4a31c0bf58a61c017e0b23e5
Thanapol Rojanapanpat <>
2023-01-26 11:33:50 +0700

    bc preview player

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div style="max-width: 960px;">
+      <style>
+        video-js.video-js.vjs-fluid:not(.vjs-audio-only-mode) {
+          padding-top: 56.25%;
+        }
+      </style>
+      <video-js
+          data-account="6415566492001"
+          data-player="Z4sz5QN9Y"
+          data-embed="default"
+          controls=""
+          data-application-id=""
+          class="vjs-fluid"></video-js>
+      <div class="vjs-playlist"></div>
+    </div>
+    <script
+        src="//players.brightcove.net/6415566492001/Z4sz5QN9Y_default/index.min.js">
+    </script>
   </div>
 </template>
