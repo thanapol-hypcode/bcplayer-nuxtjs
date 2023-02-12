2eb9f908320e100a36ca0e6db656b6f7a858f9d0
Thanapol Rojanapanpat <>
2023-02-12 22:37:31 +0700

    set playlist item and start time

--- app.vue
+++ app.vue
@@ -2,53 +2,53 @@
   <div class="mt-5 md:col-span-2 md:mt-0">
     <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
 
-      <div class="grid grid-cols-3 gap-6">
-        <div class="col-span-3 sm:col-span-2">
-          <label for="playerUrl" class="block text-sm font-medium text-gray-700">BC Player Loader</label>
-          <div class="mt-1 flex rounded-md shadow-sm">
-            <input type="text" name="playerUrl" v-model="playerUrl"
-                   class="block w-full flex-1
-                   rounded-none
-                   rounded-r-md
-                   border-gray-700
-                   focus:border-indigo-500
-                   focus:ring-indigo-500
-                   sm:text-sm"/>
-          </div>
-        </div>
-      </div>
+      <!--      <div class="grid grid-cols-3 gap-6">-->
+      <!--        <div class="col-span-3 sm:col-span-2">-->
+      <!--          <label for="playerUrl" class="block text-sm font-medium text-gray-700">BC Player Loader</label>-->
+      <!--          <div class="mt-1 flex rounded-md shadow-sm">-->
+      <!--            <input type="text" name="playerUrl" v-model="playerUrl"-->
+      <!--                   class="block w-full flex-1-->
+      <!--                   rounded-none-->
+      <!--                   rounded-r-md-->
+      <!--                   border-gray-700-->
+      <!--                   focus:border-indigo-500-->
+      <!--                   focus:ring-indigo-500-->
+      <!--                   sm:text-sm"/>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--      </div>-->
 
-      <div class="grid grid-cols-3 gap-6">
-        <div class="col-span-3 sm:col-span-2">
-          <label for="accountId" class="block text-sm font-medium text-gray-700">Account Id</label>
-          <div class="mt-1 flex rounded-md shadow-sm">
-            <input type="text" name="accountId" v-model="accountId"
-                   class="block w-full flex-1
-                   rounded-none
-                   rounded-r-md
-                   border-gray-700
-                   focus:border-indigo-500
-                   focus:ring-indigo-500
-                   sm:text-sm"/>
-          </div>
-        </div>
-      </div>
+      <!--      <div class="grid grid-cols-3 gap-6">-->
+      <!--        <div class="col-span-3 sm:col-span-2">-->
+      <!--          <label for="accountId" class="block text-sm font-medium text-gray-700">Account Id</label>-->
+      <!--          <div class="mt-1 flex rounded-md shadow-sm">-->
+      <!--            <input type="text" name="accountId" v-model="accountId"-->
+      <!--                   class="block w-full flex-1-->
+      <!--                   rounded-none-->
+      <!--                   rounded-r-md-->
+      <!--                   border-gray-700-->
+      <!--                   focus:border-indigo-500-->
+      <!--                   focus:ring-indigo-500-->
+      <!--                   sm:text-sm"/>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--      </div>-->
 
-      <div class="grid grid-cols-3 gap-6">
-        <div class="col-span-3 sm:col-span-2">
-          <label for="playerId" class="block text-sm font-medium text-gray-700">Player Id</label>
-          <div class="mt-1 flex rounded-md shadow-sm">
-            <input type="text" name="playerId" v-model="playerId"
-                   class="block w-full flex-1
-                   rounded-none
-                   rounded-r-md
-                   border-gray-700
-                   focus:border-indigo-500
-                   focus:ring-indigo-500
-                   sm:text-sm"/>
-          </div>
-        </div>
-      </div>
+      <!--      <div class="grid grid-cols-3 gap-6">-->
+      <!--        <div class="col-span-3 sm:col-span-2">-->
+      <!--          <label for="playerId" class="block text-sm font-medium text-gray-700">Player Id</label>-->
+      <!--          <div class="mt-1 flex rounded-md shadow-sm">-->
+      <!--            <input type="text" name="playerId" v-model="playerId"-->
+      <!--                   class="block w-full flex-1-->
+      <!--                   rounded-none-->
+      <!--                   rounded-r-md-->
+      <!--                   border-gray-700-->
+      <!--                   focus:border-indigo-500-->
+      <!--                   focus:ring-indigo-500-->
+      <!--                   sm:text-sm"/>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--      </div>-->
 
 
       <div class="grid grid-cols-3 gap-6">
@@ -56,7 +56,7 @@
           <label for="setting" class="block text-sm font-medium text-gray-700">Video Id</label>
           <div class="mt-1 flex rounded-md shadow-sm">
             <textarea name="setting" v-model="setting"
-                      placeholder=''
+                      placeholder='player loader config'
                       class="mt-1 block w-full
                       rounded-md
                       border-gray-700
@@ -64,6 +64,38 @@
                       focus:border-indigo-500
                       focus:ring-indigo-500
                       sm:text-sm h-64"/>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-3 gap-6">
+        <div class="col-span-3 sm:col-span-2">
+          <label for="playIdx" class="block text-sm font-medium text-gray-700">Play Idx</label>
+          <div class="mt-1 flex rounded-md shadow-sm">
+            <input type="text" name="playIdx" v-model="playIdx"
+                   class="block w-full flex-1
+                   rounded-none
+                   rounded-r-md
+                   border-gray-700
+                   focus:border-indigo-500
+                   focus:ring-indigo-500
+                   sm:text-sm"/>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-3 gap-6">
+        <div class="col-span-3 sm:col-span-2">
+          <label for="playTime" class="block text-sm font-medium text-gray-700">Play Time</label>
+          <div class="mt-1 flex rounded-md shadow-sm">
+            <input type="text" name="playTime" v-model="playTime"
+                   class="block w-full flex-1
+                   rounded-none
+                   rounded-r-md
+                   border-gray-700
+                   focus:border-indigo-500
+                   focus:ring-indigo-500
+                   sm:text-sm"/>
           </div>
         </div>
       </div>
@@ -87,47 +119,90 @@
 </template>
 <script>
 import {loadScript} from "vue-plugin-load-script";
+import brightcovePlayerLoader from '@brightcove/player-loader';
 
 export default {
   data() {
 
     return {
-      playerUrl: 'https://apis.support.brightcove.com/assets/js/playback/brightcove-player-loader.min.js',
-      accountId: '',
-      playerId: '',
-      setting: ``,
+      setting: `{
+  "accountId": "",
+  "playerId": "Z4sz5QN9Y",
+  "playlistId": "1757635339688542008",
+  "embedOptions": { "responsive": true, "unminified": true, "playlist": false }
+}`,
+      playIdx: 1,
+      playTime: 10,
     }
   },
   methods: {
     play() {
-      loadScript(this.playerUrl).then(() => {
-        try {
-          // const vdoId = JSON.parse(this.setting)
-          const vdoId = this.setting;
-          console.log(vdoId);
+      try {
+        // console.log(this.setting);
+        const config = JSON.parse(this.setting)
+        // const { vdoId, playlistId, ep, time } = this.setting;
 
-          this.buildPlayer(vdoId)
-        } catch (e) {
-          console.log(window);
-          console.error(e)
-        }
-      })
+        this.buildPlayer(config, parseInt(this.playIdx), parseInt(this.playTime))
+      } catch (e) {
+        console.log(window);
+        console.error(e)
+      }
+      // loadScript(this.playerUrl).then(() => {
+      //   try {
+      //     console.log(this.setting);
+      //     const config = JSON.parse(this.setting)
+      //     // const { vdoId, playlistId, ep, time } = this.setting;
+      //
+      //     this.buildPlayer(config)
+      //   } catch (e) {
+      //     console.log(window);
+      //     console.error(e)
+      //   }
+      // })
     },
-    buildPlayer(video_id) {
+    buildPlayer(config, playIdx, playTime) {
       const player_container = document.getElementById('player_container');
-      const brightcovePlayerLoader = window['brightcovePlayerLoader'];
+      // const brightcovePlayerLoader = window['brightcovePlayerLoader'];
+
+      // const {vdoId, playlistId, ep, time} = config;
+
+      const params = {
+        ...config,
+        refNode: player_container,
+        embedId: 'default',
+      }
+      console.log(params);
+      console.log(playIdx);
+      console.log(playTime);
 
       player_container.innerHTML = ''
-      brightcovePlayerLoader({
-        refNode: player_container,
-        accountId: this.accountId,
-        playerId: this.playerId,
-        videoId: video_id,
-        embedId: 'default',
-      }).then(function (success) {
-        console.log('player loaded');
+
+      brightcovePlayerLoader(params).then(function (success) {
+        const player = success.ref;
+        console.log(`player: `, player);
+        player.ready(function () {
+          console.log(`ready:`, player.playlist());
+        })
+        player.on('loadedmetadata',function(){
+          console.log(`loadedmetadata:`, player.playlist());
+
+          if (playIdx > 0) {
+            console.log(`currentItem: `, player.playlist?.currentItem());
+            player.playlist?.currentItem(playIdx);
+            console.log(`currentItem: `, player.playlist?.currentItem());
+          }
+          if (playTime > 0) {
+            console.log(player.currentTime());
+            player.currentTime(playTime);
+            console.log(player.currentTime());
+          }
+
+          player.play();
+        });
+
+
       }).catch(function (error) {
-        var p = document.createElement('p');
+        let p = document.createElement('p');
         p.textContent =
             'Sorry - the player could not be loaded. Please try again later.';
         player_container.appendChild(p);
